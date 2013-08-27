705d5a928864b736c87f20012dc9291f059583c7
sakura <sakura@iti.co.jp>
2013-08-27 11:32:12 +0900

    iOS Auto Layout sample
    first commit

---
+++ AutoLayout/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AutoLayout
+//
+//  Created by sakura on 2013/08/21.
+//  Copyright (c) 2013年 ITI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AutoLayout/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AutoLayout
+//
+//  Created by sakura on 2013/08/21.
+//  Copyright (c) 2013年 ITI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
